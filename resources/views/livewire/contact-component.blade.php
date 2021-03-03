0b58306a8074da278d80202665caeaa834d87c33
NainAceroMamani <a_nacerom@unjbg.edu.pe>
2021-03-03 11:59:12 -0500

    update 03/03/2021

--- resources/views/livewire/contact-component.blade.php
+++ resources/views/livewire/contact-component.blade.php
@@ -11,35 +11,52 @@
 				</ul>
 			</div>
 			<div class="row">
-				<div class="main-content-area">
-					<div class="wrap-contacts">
-						<div class="col-lg-8 col-sm-8 col-md-8 col-xs-12">
-                            <div class="contact-box contact-info">
-                                <div class="wrap-map">
-                                    <div class="mercado-google-maps"
-                                        id="az-google-maps57341d9e51968"
-                                        data-hue=""
-                                        data-lightness="1"
-                                        data-map-style="2"
-                                        data-saturation="-100"
-                                        data-modify-coloring="false"
-                                        data-title_maps="Agrovic TORRES"
-                                        data-phone="(052) 424 394"
-                                        data-email="agrovid@torres.com"
-                                        data-address="Prolongación Patricio Melendez 583 – Tacna"
-                                        data-longitude="-70.249504"
-                                        data-latitude="-18.007446"
-                                        data-pin-icon=""
-                                        data-zoom="16"
-                                        data-map-type="ROADMAP"
-                                        data-map-height="263">
-                                    </div>
-                                </div>
+				<div class=" main-content-area">
+					<div class="wrap-contacts ">
+						<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
+							<div class="contact-box contact-form">
+								<h2 class="box-title">Leave a Message</h2>
+								<form action="#" method="get" name="frm-contact">
+
+									<label for="name">Name<span>*</span></label>
+									<input type="text" value="" id="name" name="name" >
+
+									<label for="email">Email<span>*</span></label>
+									<input type="text" value="" id="email" name="email" >
+
+									<label for="phone">Number Phone</label>
+									<input type="text" value="" id="phone" name="phone" >
+
+									<label for="comment">Comment</label>
+									<textarea name="comment" id="comment"></textarea>
+
+									<input type="submit" name="ok" value="Submit" >
+
+								</form>
 							</div>
 						</div>
-						<div class="col-lg-4 col-sm-4 col-md-4 col-xs-12">
+						<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
 							<div class="contact-box contact-info">
-
+								<div class="wrap-map">
+									<div class="mercado-google-maps"
+										 id="az-google-maps57341d9e51968"
+										 data-hue=""
+										 data-lightness="1"
+										 data-map-style="2"
+										 data-saturation="-100"
+										 data-modify-coloring="false"
+										 data-title_maps="Kute themes"
+										 data-phone="088-465 9965 02"
+										 data-email="kutethemes@gmail.com"
+										 data-address="Z115 TP. Thai Nguyen"
+										 data-longitude="-0.120850"
+										 data-latitude="51.508742"
+										 data-pin-icon=""
+										 data-zoom="16"
+										 data-map-type="ROADMAP"
+										 data-map-height="263">
+									</div>
+								</div>
 								<h2 class="box-title">DETALLES DEL CONTACTO</h2>
 								<div class="wrap-icon-box">
 
